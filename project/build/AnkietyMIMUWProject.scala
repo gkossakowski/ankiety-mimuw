fc3211f56bc5f7af69caf035e9b1a40ef6bfea63
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2010-05-07 21:48:22 +0200

    Added dependency on postgresql jdbc driver.

--- project/build/AnkietyMIMUWProject.scala
+++ project/build/AnkietyMIMUWProject.scala
@@ -1,3 +1,5 @@
 import sbt._
 
-class AnkietyMIMUWProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin
+class AnkietyMIMUWProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+  val postgresql = "postgresql" % "postgresql" % "8.4-701.jdbc4" 
+}
